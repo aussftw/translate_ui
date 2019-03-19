c311ba2eb125f4395c3de56e396213f372975bb4
aussftw <shagronaus@gmail.com>
2019-03-19 07:16:49 +0200

    cleaning app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,20 @@
 import React from "react";
 import UserCreate from "./UserCreate";
-import LanguageContext from "../contexts/LanguageContext";
+import { LanguageStore } from "../contexts/LanguageContext";
 import ColorContext from "../contexts/ColorContext";
+import LanguageSelector from "./LanguageSelect";
 
 class App extends React.Component {
-  state = { language: "english" };
-
-  onLanguageChange = language => {
-    this.setState({ language: language });
-  };
-
   render() {
     return (
       <div className="ui container ">
-        <div>
-          Select a language:
-          <i
-            onClick={() => this.onLanguageChange("english")}
-            className="flag us"
-          />
-          <i
-            onClick={() => this.onLanguageChange("dutch")}
-            className="flag nl"
-          />
+        <LanguageStore>
+          <LanguageSelector />
+
           <ColorContext.Provider value="violet">
-            <LanguageContext.Provider value={this.state.language}>
-              <UserCreate />
-            </LanguageContext.Provider>
+            <UserCreate />
           </ColorContext.Provider>
-        </div>
+        </LanguageStore>
       </div>
     );
   }
